6d3b5156f5874b312d6d79d6c36f6d2c61284fd5
juliaisaeva <julia.balashova@arcadia.spb.ru>
2018-05-15 22:49:29 +0300

    FirstLesson commit

---
+++ ArcadiaCourse/ArcadiaCourse/FirstLesson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcadiaCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
