659e9294954c384640bd84090987cbc60ba65d00
Julia Balashova <julia.balashova@arcadia.spb.ru>
2018-05-28 13:28:15 +0300

    rename class main in FirstLesson

--- ArcadiaCourse/ArcadiaCourse/Lessons/FirstLesson.cs
+++ ArcadiaCourse/ArcadiaCourse/Lessons/FirstLesson.cs
@@ -4,7 +4,7 @@ namespace BTL.Arcadia.QAAuto
 {
     internal class FirstLesson
     {
-        private static void FL()
+        private static void FirstL()
         {
             Console.WriteLine("This is my first lesson");
             Console.ReadKey();
